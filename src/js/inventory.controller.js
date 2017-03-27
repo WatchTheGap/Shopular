1c8dddc1e04ab8ee59139d6d2f2fa845e2d577ab
Sara Basile <sarabasile@Saras-Air.lan>
2017-03-27 16:23:17 -0400

    added docblocks

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -3,7 +3,9 @@
 
   angular.module('inventory')
     .controller('InventoryController', InventoryController);
-
+    /**
+     * Constructs controller for populating inventory data in a table.
+     */
     function InventoryController() {
       let vm = this;
 
@@ -23,6 +25,12 @@
       ];
 
       vm.tax = 0.0575;
+      /**
+       * Calculates the final price of an item by subtracting any applicable
+       * discounts and adding sales tax.
+       * @param  {Object} item Individual objects from the InventoryList array
+       * @return {Number}       Final price of each item after discounts & tax.
+       */
       vm.finalPrice = function finalPrice(item) {
         let price = (item.price - item.discount);
         let tax = price * vm.tax;

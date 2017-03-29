8a19d4d7b6db6e1cdc0c0b9d7ca8224fdd77c0ec
Sara Basile <sarabasile@Saras-Air.lan>
2017-03-29 13:36:10 -0400

    added docblocks

--- src/js/item.service.js
+++ src/js/item.service.js
@@ -3,21 +3,51 @@
 
   angular.module('inventory').factory('ItemService', ItemService);
 
+
+  /**
+   * Creates the Item Service model for handling inventory items
+   */
   function ItemService() {
 
     let items = JSON.parse(localStorage.getItem('items')) || [];
 
+    /**
+     * Retrieves all items in the inventory
+     * @return {Array} Array of inventory items
+     */
     function getAllItems() {
       return items;
     }
-    
-    function addNewItem() {
 
+    /**
+     * Adds a new item to the inventory
+     * @return {void}
+     */
+    function addItem() {
+      if (typeof(item) !== 'object' ||
+         typeof(item.name) !== 'string' ||
+         typeof(item.price) !== 'number' ||
+         typeof(item.quantity) !== 'number' ||
+         typeof(item.color) !== 'string' ||
+         typeof(item.discount) !== 'number'){
+          //add conversion to numbers - all form data is strings
+        return;
+      }
+
+      items.push({
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+        color: item.color,
+        discount: item.discount
+      });
+      localStorage.setItem('items', angular.toJson(items));
     }
 
-    function updateAll() {
-
-    }
+    return {
+      addItem: addItem,
+      getAllItems: getAllItems,
+    };
   }
 
 }());

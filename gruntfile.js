46544bcdc02433892f2d9e1233877230a30d267b
Sara Basile <sarabasile@Saras-Air.lan>
2017-03-30 16:33:02 -0400

    set up start of gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,34 @@
 'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    karma: {
+
+      all: {
+
+        options: {
+          frameworks: ['mocha', 'chai'],
+          browsers: ['Chrome'],
+          files: [
+            'node_modules/angular/angular.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'src/js/inventory.module.js',
+            'src/js/**/*.js',
+            'tests/**/*.spec.js'
+          ]
+        }
+      }
+    }
+
+
+
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.registerTask('build', ['karma']);
+
+};
